64b1ebd47faa437116148c8f7e43fc7cbf272759
Arthur Oliveira <arthur.oliveira@hotmail.com.br>
2020-02-21 23:09:23 -0300

    Implements basic variable control

--- web/src/app/generator/generator.component.ts
+++ web/src/app/generator/generator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { IVariable } from '@typings/Variable';
 
 @Component({
   selector: 'cg-generator',
@@ -7,13 +8,33 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./generator.component.sass']
 })
 export class GeneratorComponent implements OnInit {
-  variables = this.formBuilder.array([
-    this.formBuilder.control(''),
-  ]);
+  variables = this.formBuilder.array([]);
 
   constructor(
     private formBuilder: FormBuilder,
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.addVariable();
+  }
+
+  addVariable(params: IVariable = {
+    name: '',
+    value: '',
+  }): FormGroup {
+    const formGroup = this.formBuilder.group(params);
+    this.variables.push(formGroup);
+    return formGroup;
+  }
+
+  removeVariable(
+    variable: FormGroup,
+    index: number,
+  ) {
+    const variableName = variable.get('name').value;
+    if (!confirm(`Remove variable ${variableName}?`)) {
+      return;
+    }
+    this.variables.removeAt(index);
+  }
 }
