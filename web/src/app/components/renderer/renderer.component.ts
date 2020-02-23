49e895b12f12873d3a77b640f8559ae1186eaa0f
Arthur Oliveira <arthur.oliveira@hotmail.com.br>
2020-02-23 19:35:23 -0300

    Variables reduce enhancement
    - Reduces how often the variables are reduced on renderer component

--- web/src/app/components/renderer/renderer.component.ts
+++ web/src/app/components/renderer/renderer.component.ts
@@ -17,40 +17,51 @@ export class RendererComponent implements OnInit {
   @Input() template: BehaviorSubject<ITemplate>;
   @Input() variables: FormArray;
 
+  private variablesReduced = new BehaviorSubject<any>({});
+
   processedTemplate = new BehaviorSubject<string>('');
 
   constructor(
   ) { }
 
   ngOnInit(): void {
-    this._watchTemplateChanges();
+    this._reduceVariables();
+    this._watchInputChanges();
   }
 
-  private _watchTemplateChanges(): Subscription {
+  private _watchInputChanges(): Subscription {
+    const variablesChangesSubscription = this.variables.valueChanges.subscribe(() => {
+      return this._reduceVariables();
+    });
+
     return merge(
       this.template,
-      this.variables.valueChanges,
+      this.variablesReduced,
     ).pipe(
       debounceTime(environment.debounceTime),
     ).subscribe(() => {
       this._renderTemplate();
-    });
+    }).add(variablesChangesSubscription);
   }
 
-  private _renderTemplate(): string {
-    const variables = this.variables.value.reduce((acc, variable: IVariable) => {
+  private _reduceVariables(): any {
+    const variablesReduced = this.variables.value.reduce((acc, variable: IVariable) => {
       acc[variable.name] = variable.value;
       return acc;
     }, {});
+    this.variablesReduced.next(variablesReduced);
+    return variablesReduced;
+  }
 
+  private _renderTemplate(): string {
     try {
-      const template = Handlebars.compile(this.template.value.text)(variables);
+      const template = Handlebars.compile(this.template.value.text)(this.variablesReduced.value);
       this.processedTemplate.next(template);
       return template;
     } catch (error) {
       console.error(error);
     }
-
+    return this.processedTemplate.value;
   }
 
 }
