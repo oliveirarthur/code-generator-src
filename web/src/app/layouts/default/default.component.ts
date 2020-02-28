d43e5348736e81af933408a123919a3494743dc5
Arthur Oliveira <arthur.oliveira@hotmail.com.br>
2020-02-27 22:44:38 -0300

    Implements data export

--- web/src/app/layouts/default/default.component.ts
+++ web/src/app/layouts/default/default.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { DataService } from '@app/services/data.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'cg-default',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
-  constructor() { }
+  exportURL = new BehaviorSubject<SafeUrl>('');
+
+  constructor(
+    private readonly dataSvc: DataService,
+    private readonly domSanitizer: DomSanitizer,
+  ) { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this._watchDataChanges();
+    });
+  }
+
+  private _watchDataChanges() {
+    this.dataSvc.data.subscribe(() => {
+      const data = this.dataSvc.exportAll();
+      const dataJSON = JSON.stringify(data, null, 2);
+      const fileURI = 'data:text/json;charset=utf-8,' + encodeURIComponent(dataJSON);
+      const fileURISanitized = this.domSanitizer.bypassSecurityTrustUrl(fileURI);
+      this.exportURL.next(fileURISanitized);
+    });
   }
 
 }
--- web/src/app/services/data.service.ts
+++ web/src/app/services/data.service.ts
@@ -2,20 +2,35 @@ import { Injectable } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ITab } from '@typings/Tab';
 import { ITemplate } from '@typings/Template';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, merge } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  variables = this.formBuilder.array([]);
+  tabs = new BehaviorSubject<Array<ITab>>([]);
   template = new BehaviorSubject<ITemplate>({
     text: `Test text {{ a }}`,
   } as ITemplate);
-  tabs = new BehaviorSubject<Array<ITab>>([]);
+  variables = this.formBuilder.array([]);
+
+  data = merge(
+    this.tabs,
+    this.template,
+    this.variables,
+  );
 
   constructor(
     private formBuilder: FormBuilder,
   ) { }
+
+  exportAll() {
+    return {
+      variables: this.variables.value,
+      template: this.template.value,
+      tabs: this.tabs.value,
+    };
+  }
+
 }
